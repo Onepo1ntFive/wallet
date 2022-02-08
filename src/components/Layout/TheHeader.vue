83ba241c3c032cfba6680d24f8443bd60b0e2211
Onepo1ntFive <name.diesel@gmail.com>
2022-02-09 00:36:01 +0300

    Logout btn

---
+++ src/components/Layout/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <logout-button></logout-button>
+</template>
+
+<script>
+import LogoutButton from "../LogoutButton.vue";
+export default {
+    components: {
+        LogoutButton,
+    },
+};
+</script>
---
+++ src/components/LogoutButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="logout">Logout</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        logout(){
+             window.localStorage.removeItem('token');
+             this.$router.push('/');
+        }
+    },
+}
+</script>
