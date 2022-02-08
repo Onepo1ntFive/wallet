a7d5e915ecff91ba41ed472db216d2722a2b2253
Onepo1ntFive <name.diesel@gmail.com>
2022-02-09 00:36:14 +0300

    Auth form

--- src/components/Auth/AuthForm.vue
+++ src/components/Auth/AuthForm.vue
@@ -1,16 +1,60 @@
 <template>
     <h3>AuthForm</h3>
-    <form>
+    <form @submit.prevent="submitForm">
         <div>
-            <label for="email">E-mail</label>
-            <input type="email" placeholder="Email" />
+            <input v-model.trim="login" type="email" placeholder="Email" />
         </div>
         <div>
-            <label for="password">Password</label>
-            <input type="password" placeholder="Email" />
+            <input
+                v-model.trim="password"
+                type="password"
+                placeholder="Password"
+            />
         </div>
         <div>
             <button>Login</button>
         </div>
     </form>
 </template>
+
+<script>
+import { submitLoginForm, CheckUserAuthorized } from "../../api/api.js";
+export default {
+    data() {
+        return {
+            login: null,
+            password: null,
+        };
+    },
+    methods: {
+        submitForm() {
+            submitLoginForm(
+                this.login,
+                this.password,
+                this.successRequest,
+                this.errorRequest
+            );
+        },
+        successRequest(response) {
+            const token = response.data.token;
+            window.localStorage.setItem("token", token);
+            this.$router.push("/main");
+        },
+        errorRequest(error) {
+            console.log(error);
+        },
+        successUserAuthorized() {
+            this.$router.push("/main");
+        },
+        errorSuccessAuthorized() {
+            this.$router.push("/");
+        },
+    },
+    created() {
+        CheckUserAuthorized(
+            this.successUserAuthorized,
+            this.errorSuccessAuthorized
+        );
+    },
+};
+</script>
